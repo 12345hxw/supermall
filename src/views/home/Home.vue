ead77fbd91d1d75f10603f68ffcaf1bb7f215f75
黄鑫伟 <2575014210@qq.com>
2021-08-05 11:02:41 +0800

    第四次更新项目

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,14 +1,19 @@
 <template>
   <div id="home" class="wrapper">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-  
-      <home-swiper :banners="banners"/>
-      <recommend-view :recommends="recommends"/>
-      <feature-view/>
-      <tab-control class="tab-control"
-                   :titles="['流行', '新款', '精选']"
-                   @tabClick="tabClick"/>
-      <good-list :goods="showGoods"/>
+      <Scroll class="content" ref="scroll" :probe-type="3"
+       @scroll="contentScroll" :pull-up-load="true" @pullingUp="loadMore">
+        <home-swiper :banners="banners"/>
+        <recommend-view :recommends="recommends"/>
+        <feature-view/>
+        <tab-control class="tab-control"
+                    :titles="['流行', '新款', '精选']"
+                    @tabClick="tabClick"/>
+        <good-list :goods="showGoods"/>
+      </Scroll>
+      <!-- 监听组件的原生事件 -->
+      <back-top @click.native="backClick" v-show="isShowBackTop"/>
+     
   
   
   </div>
@@ -18,15 +23,17 @@
   import HomeSwiper from './childComps/HomeSwiper'
   import RecommendView from './childComps/RecommendView'
   import FeatureView from './childComps/FeatureView'
+  
 
   import NavBar from 'components/common/navbar/NavBar'
   import TabControl from 'components/content/tabControl/TabControl'
   import GoodList from 'components/content/goods/GoodsList'
-  // import Scroll from 'components/common/scroll/Scroll'
-  // import BackTop from 'components/content/backTop/BackTop'
+  import Scroll from 'components/common/scroll/Scroll'
+  import BackTop from 'components/content/backTop/BackTop'
 
   import { getHomeMultidata, getHomeGoods } from "network/home"
 
+ 
   export default {
     name: "Home",
     components: {
@@ -36,8 +43,8 @@
       NavBar,
       TabControl,
       GoodList,
-      // Scroll,
-      // BackTop
+      Scroll,
+      BackTop
     },
     data() {
       return {
@@ -100,7 +107,21 @@
           this.goods[type].list.push(...res.data.list)
           this.goods[type].page += 1
 
+          this.$refs.scroll.finishPullUp()
         })
+      },
+      backClick(){
+        this.$refs.scroll.scrollTo(0,0)
+      },
+      contentScroll(position){
+        // console.log(position)
+        this.isShowBackTop=(-position.y) > 1000
+
+      },
+      loadMore(){
+        // console.log("上拉加载成功")
+        this.getHomeGoods(this.currentType)
+        // this.$refs.scroll.finishPushUp()
       }
     }
   }
@@ -108,7 +129,7 @@
 
 <style scoped>
   #home {
-    /*padding-top: 44px;*/
+    padding-top: 44px;
     height: 100vh;
     position: relative;
   }
