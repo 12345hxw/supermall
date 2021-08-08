c7bf6457d56874a8c4ac74af2fce202c82f69b08
黄鑫伟 <2575014210@qq.com>
2021-08-08 15:18:10 +0800

    项目完工

--- src/main.js
+++ src/main.js
@@ -3,11 +3,22 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import toast from 'components/common/toast'
+import FastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
 Vue.config.productionTip = false
 
 Vue.prototype.$bus=new Vue()
 //安装toast插件
+
+
+//解决移动端300Ms延时
+FastClick.attach(document.body)
 Vue.use(toast)
+
+//使用懒加载的插件
+Vue.use(VueLazyLoad,{
+  loading:require('./assets/img/common/placeholder.png')
+})
 new Vue({
   render: h => h(App),
   router,
--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -1,126 +1,79 @@
 <template>
-  <div>
-    <scroll class="content">
-      <ul>
-        <li>信息1</li>
-        <li>信息2</li>
-        <li>信息3</li>
-        <li>信息4</li>
-        <li>信息5</li>
-        <li>信息6</li>
-        <li>信息7</li>
-        <li>信息8</li>
-        <li>信息9</li>
-        <li>信息10</li>
-        <li>信息11</li>
-        <li>信息12</li>
-        <li>信息13</li>
-        <li>信息14</li>
-        <li>信息15</li>
-        <li>信息16</li>
-        <li>信息17</li>
-        <li>信息18</li>
-        <li>信息19</li>
-        <li>信息20</li>
-        <li>信息21</li>
-        <li>信息22</li>
-        <li>信息23</li>
-        <li>信息24</li>
-        <li>信息25</li>
-        <li>信息26</li>
-        <li>信息27</li>
-        <li>信息28</li>
-        <li>信息29</li>
-        <li>信息30</li>
-        <li>信息31</li>
-        <li>信息32</li>
-        <li>信息33</li>
-        <li>信息34</li>
-        <li>信息35</li>
-        <li>信息36</li>
-        <li>信息37</li>
-        <li>信息38</li>
-        <li>信息39</li>
-        <li>信息40</li>
-        <li>信息41</li>
-        <li>信息42</li>
-        <li>信息43</li>
-        <li>信息44</li>
-        <li>信息45</li>
-        <li>信息46</li>
-        <li>信息47</li>
-        <li>信息48</li>
-        <li>信息49</li>
-        <li>信息50</li>
-        <li>信息51</li>
-        <li>信息52</li>
-        <li>信息53</li>
-        <li>信息54</li>
-        <li>信息55</li>
-        <li>信息56</li>
-        <li>信息57</li>
-        <li>信息58</li>
-        <li>信息59</li>
-        <li>信息60</li>
-        <li>信息61</li>
-        <li>信息62</li>
-        <li>信息63</li>
-        <li>信息64</li>
-        <li>信息65</li>
-        <li>信息66</li>
-        <li>信息67</li>
-        <li>信息68</li>
-        <li>信息69</li>
-        <li>信息70</li>
-        <li>信息71</li>
-        <li>信息72</li>
-        <li>信息73</li>
-        <li>信息74</li>
-        <li>信息75</li>
-        <li>信息76</li>
-        <li>信息77</li>
-        <li>信息78</li>
-        <li>信息79</li>
-        <li>信息80</li>
-        <li>信息81</li>
-        <li>信息82</li>
-        <li>信息83</li>
-        <li>信息84</li>
-        <li>信息85</li>
-        <li>信息86</li>
-        <li>信息87</li>
-        <li>信息88</li>
-        <li>信息89</li>
-        <li>信息90</li>
-        <li>信息91</li>
-        <li>信息92</li>
-        <li>信息93</li>
-        <li>信息94</li>
-        <li>信息95</li>
-        <li>信息96</li>
-        <li>信息97</li>
-        <li>信息98</li>
-        <li>信息99</li>
-        <li>信息100</li>
-      </ul>
-    </scroll>
+  <div id="profile">
+    <nav-bar class="nav-bar">
+      <div slot="center">商城</div>
+    </nav-bar>
+    <user-info></user-info>
+    <div class="ppp">
+      <detail-info>
+        <div slot="name">
+          0.00
+          <span style="color: #666666; font-weight: normal; font-size: 15px"
+            >元</span
+          >
+        </div>
+        <div slot="value">我的余额</div>
+      </detail-info>
+      <detail-info>
+        <div slot="name">
+          0
+          <span style="color: #666666; font-weight: normal; font-size: 15px"
+            >个</span
+          >
+        </div>
+        <div slot="value">我的优惠</div>
+      </detail-info>
+      <detail-info>
+        <div slot="name">
+          0
+          <span style="color: #666666; font-weight: normal; font-size: 15px"
+            >分</span
+          >
+        </div>
+        <div slot="value">我的积分</div>
+      </detail-info>
+    </div>
+    <list-view :list-data="orderList" class="order-list"></list-view>
+    <list-view :list-data="serviceList" class="service-list"></list-view>
   </div>
 </template>
 
 <script>
-import Scroll from 'components/common/scroll/Scroll'
+import NavBar from "components/common/navbar/NavBar";
+import UserInfo from "./childComps/UserInfo";
+import detailInfo from "./childComps/detailInfo";
+import ListView from "./childComps/ListView";
 export default {
-  name:"profile",
-  components:{
-    Scroll
-  }
-}
+  name: "Profile",
+  components: {
+    NavBar,
+    UserInfo,
+    detailInfo,
+    ListView,
+  },
+  data: function () {
+    return {
+      orderList: [
+        { icon: "#order", iconColor: "#ff8198", info: "我的消息" },
+        { icon: "#point", iconColor: "#fc7b53", info: "积分商城" },
+        { icon: "#vip", iconColor: "#ffc636", info: "会员卡" },
+      ],
+      serviceList: [
+        { icon: "#service", iconColor: "#ff8198", info: "我的购物车" },
+        { icon: "#download", iconColor: "#ff8198", info: "下载购物APP" },
+      ],
+    };
+  },
+  mounted: function () {},
+};
 </script>
 
 <style scoped>
-  .content{
-    height:100px;
-    background-color: red;
-    overflow: hidden;
-  }
+.nav-bar {
+  background-color: #ff8198;
+  color: white;
+}
+.ppp {
+  display: flex;
+}
 </style>
